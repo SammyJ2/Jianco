7cadb91967a1354a47163e52e6229471c30d1b36
SammyJ2 <138932483+SammyJ2@users.noreply.github.com>
2023-07-11 13:37:25 -0700

    Delete jianco directory

--- jianco/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from django.contrib import admin
-from .models import MNR
-
-admin.site.register(MNR)
-
--- jianco/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class JiancoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jianco'
